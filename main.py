c87495b86157a430238a27ca77139e4cfe059361
Mohammad Zrar <105074619+mohammad-zrar@users.noreply.github.com>
2023-02-06 22:12:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -63,5 +63,5 @@ def cafes():
     return render_template('cafes.html', cafes=list_of_rows)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
